cf210d7f123973c5844c74d99d033b9a768d48a8
Toriqul-Leon <yeitoriqul@gmail.com>
2022-06-08 10:25:03 +0600

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Default from "./components/Default";
+import Cart from "./components/Cart";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="productList" element={<ProductList />} />
+        <Route path="details" element={<Details />} />
+        <Route path="details" element={<Details />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="*" element={<Default />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Default.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Default = () => {
+  return (
+    <div>
+      <h2>Page Note Found</h2>
+    </div>
+  );
+};
+
+export default Default;
