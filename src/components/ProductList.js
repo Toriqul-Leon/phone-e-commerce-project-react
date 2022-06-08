e2104b36f6a3f8dfeebaf6eb4aa27897be633504
Toriqul-Leon <yeitoriqul@gmail.com>
2022-06-09 00:01:01 +0600

    Context added

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,26 @@
-import React from "react";
-import Product from "./Product";
+import React, { useState } from "react";
+import Title from "./Title";
+import { storeProducts } from "../data";
+import { ProductConsumer } from "../Context";
 
 const ProductList = () => {
+  const [products, setProducts] = useState(storeProducts);
+  console.log(products);
   return (
     <div>
-      <Product></Product>
+      <div className="py-5">
+        <div className="container">
+          <Title name="our" title="Products"></Title>
+          <div className="row">
+            <ProductConsumer>
+              {(value) => {
+                return <h1>{value}</h1>;
+              }}
+            </ProductConsumer>
+          </div>
+        </div>
+      </div>
+      {/* <Product /> */}
     </div>
   );
 };
