6617e9285146f90e6d6b94d2d7baaea537698653
Toriqul-Leon <yeitoriqul@gmail.com>
2022-06-14 15:22:30 +0600

    404 added

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,8 +2,21 @@ import React from "react";
 
 const Default = () => {
   return (
-    <div>
-      <h2>Page Note Found</h2>
+    <div className="container">
+      <div className="row">
+        <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+          <h1>
+            <strong>404</strong>
+          </h1>
+          <h1>
+            <strong>page not found</strong>
+          </h1>
+          <h3>
+            the page you are looking for might have been removed had its name
+            changed or is temporarily unavailable
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
